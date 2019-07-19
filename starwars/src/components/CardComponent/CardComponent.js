0ddd98edf58765237d2075f05f1b133b64b77398
TheHaunted <kevin_lopez@live.com>
2019-07-19 12:39:56 -0500

    filled page with character cards

--- starwars/src/components/CardComponent/CardComponent.js
+++ starwars/src/components/CardComponent/CardComponent.js
@@ -4,8 +4,11 @@ import {Card} from 'semantic-ui-react';
 function CharacterCard(props){
     return(
         <Card>
-            <img src={props.url} />
             <h3 className="name">{props.name}</h3>
+            <Card.Description>
+                <p>Gender: {props.gender}</p>
+                <p>Eye color: {props.eyes}</p>
+            </Card.Description>
         </Card>
     )
 }
